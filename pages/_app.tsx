babe51db2c196df320bcd79fef6797faf7262d28
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-12-03 12:33:43 +0100

    Adding necessary changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
+import React from "react";
+
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
+// Head
+import Head from "next/head";
+
+// `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      {/* Meta data */}
+      <Head>
+        <meta
+          name="description"
+          content="Movies is an entertainment web app which allow you to search for your favorite movie and watch it"
+        />
+
+        <link
+          rel="shortcut icon"
+          href="./assets/favicon.png"
+          type="image/x-icon"
+        />
+
+        <title>Movies - Entertainment web app</title>
+      </Head>
+
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </React.Fragment>
+  );
 }
